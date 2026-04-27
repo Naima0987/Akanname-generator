68a6a6c15f36c571ec3c5bd995158db7f7269cb4
Naima <naimaguyo12@gmail.com>
2026-04-27 18:43:09 +0200

    Addedform event listener

---
+++ code.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("form");
+const output = document.getElementById("output");
+const error = document.getElementById("error");
+const resetBtn = document.getElementById("reset");
+
+const male = ["Ryan", "Ray", "Robin", "Ronald", "Ricky"];
+const Female = ["Naima", "Nami", "Nicole", "Nico", "Niche"];
+
+form.addEventListerner("submit", function (e) {
+    e.preventDefault();
+}

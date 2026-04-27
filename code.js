8d19e83d1d38b8b7bbff9794450f836d04aba3a0
Naima <naimaguyo12@gmail.com>
2026-04-27 20:09:22 +0200

    Linked JavaScript file and set up DOM element reference

--- code.js
+++ code.js
@@ -3,12 +3,49 @@ const output = document.getElementById("output");
 const error = document.getElementById("error");
 const resetBtn = document.getElementById("reset");
 
-const male = ["Ryan", "Ray", "Robin", "Ronald", "Ricky"];
-const Female = ["Naima", "Nami", "Nicole", "Nico", "Niche"];
+const male = ["Ryan", "Ray", "Robin", "Ronald", "Ricky", "Rex", "Ralph"];
+const female = ["Naima", "Nami", "Nicole", "Nico", "Niche", "Nora", "Nelly"];
 
-form.addEventListerner("submit"), function (e) {
+form.addEventListener("submit", function (e) {
     e.preventDefault();
-}
-const dateValue = document.getElementById("date").value;
-const gender = document.querySelector('input[name = "gender"]: checked');
 
+    const dateValue = document.getElementById("dob").value;
+    const gender = document.querySelector('input[name="gender"]:checked');
+
+    error.textContent = "";
+    output.textContent = "";
+
+    if (!dateValue) {
+        error.textContent = "⚠️ Please select your birth date";
+        return;
+    }
+
+    if (!gender) {
+        error.textContent = "⚠️ Please select your gender";
+        return;
+    }
+
+    const date = new Date(dateValue);
+    const dayIndex = date.getDay();
+
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+
+    let akanName = gender.value === "male" ? male[dayIndex] : female[dayIndex];
+
+    output.textContent = `🎉 You were born on a ${days[dayIndex]}. Your Akan name is ${akanName}`;
+});
+
+
+resetBtn.addEventListener("click", function() {
+    form.reset();
+    output.textContent = "";
+    error.textContent = "";
+});
